ded50a6676afff14ef89a2c87b32fb0d084d0ba6
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-03-30 07:24:07 +0700

    nội dung ghi chú

---
+++ Giang_nopbai/src/components/LetterForm.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function LetterForm({ onAdd }) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (text.trim() === "") return;
+    onAdd(text);
+    setText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Viết letter..."
+        value={text} 
+        onChange={(e) => setText(e.target.value)} 
+        style={{width:"100%",padding:"8px"}}
+      />
+      <button style={{marginTop:"10px"}}>
+        Gửi
+      </button>
+    </form>
+  );
+}
+
+
+export default LetterForm;
